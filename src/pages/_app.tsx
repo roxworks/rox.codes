ba818988bf778554496f5382450404a0bb7f4517
Rox Works <rox@rox.works>
2023-03-12 16:22:22 -0600

    add posthog to personal site and also GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { type AppType } from "next/app";
 
 import { trpc } from "../utils/trpc";
+import { GoogleAnalytics } from "nextjs-google-analytics";
+import { usePostHog } from "next-use-posthog";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+
+  usePostHog(process.env.NEXT_PUBLIC_POSTHOG_ID || '', {
+    api_host: "https://app.posthog.com",
+  });
+
+  return <>
+    <GoogleAnalytics trackPageViews />
+    <Component {...pageProps} />
+  </>;
 };
 
 export default trpc.withTRPC(MyApp);
