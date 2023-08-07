1e596c744b8d8284454eaf833ac66029e17ae166
Rox Codes <rox@rox.works>
2023-08-07 15:30:13 -0700

    maybe

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,38 @@ import { GoogleAnalytics } from "nextjs-google-analytics";
 import { usePostHog } from "next-use-posthog";
 
 import "../styles/globals.css";
+import Head from "next/head";
+import Script from "next/script";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 
   usePostHog(process.env.NEXT_PUBLIC_POSTHOG_ID || '', {
     api_host: "https://app.posthog.com",
   });
+/**
+ * <script>
+  var script = document.createElement("script");
+  script.src = "https://app.krunchdata.io/assets/js/k2.js";
+  script.dataset.api = "https://api.krunchdata.io/traffic/web/record";
+  script.dataset.id = "ivY21exnYiiPhw3BSpdd4jbUp6AGiZx7a0xH6Cmo2lS3po4DKvfvduV2FotbzV9M";
+  script.defer = true;
+  script.type = "module";
+
+  document.head.appendChild(script);
+  console.log("Krunch script installed");
+</script>
+ */
 
   return <>
+  <Head>
+    <Script
+      src="https://app.krunchdata.io/assets/js/k2.js"
+      data-api="https://api.krunchdata.io/traffic/web/record"
+      data-id="ivY21exnYiiPhw3BSpdd4jbUp6AGiZx7a0xH6Cmo2lS3po4DKvfvduV2FotbzV9M"
+      defer
+      type="module"
+    />
+  </Head>
     <GoogleAnalytics trackPageViews />
     <Component {...pageProps} />
   </>;
