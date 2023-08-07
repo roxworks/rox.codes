988bb1e07f5cdf57aeab8614787f3994d60b2283
Rox Codes <rox@rox.works>
2023-08-07 16:48:39 -0700

    this was the best...

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,38 +8,35 @@ import "../styles/globals.css";
 import Head from "next/head";
 import Script from "next/script";
 
-
 const MyApp: AppType = ({ Component, pageProps }) => {
 
   usePostHog(process.env.NEXT_PUBLIC_POSTHOG_ID || '', {
     api_host: "https://app.posthog.com",
   });
-  /**
-   * <script>
-    var script = document.createElement("script");
-    script.src = "https://app.krunchdata.io/assets/js/k2.js";
-    script.dataset.api = "https://api.krunchdata.io/traffic/web/record";
-    script.dataset.id = "ivY21exnYiiPhw3BSpdd4jbUp6AGiZx7a0xH6Cmo2lS3po4DKvfvduV2FotbzV9M";
-    script.defer = true;
-    script.type = "module";
-  
-    document.head.appendChild(script);
-    console.log("Krunch script installed");
-  </script>
-   */
+/**
+ * <script>
+  var script = document.createElement("script");
+  script.src = "https://app.krunchdata.io/assets/js/k2.js";
+  script.dataset.api = "https://api.krunchdata.io/traffic/web/record";
+  script.dataset.id = "ivY21exnYiiPhw3BSpdd4jbUp6AGiZx7a0xH6Cmo2lS3po4DKvfvduV2FotbzV9M";
+  script.defer = true;
+  script.type = "module";
+
+  document.head.appendChild(script);
+  console.log("Krunch script installed");
+</script>
+ */
 
   return <>
-    <script>
-      var script = document.createElement("script");
-      script.src = "https://app.krunchdata.io/assets/js/k2.js";
-      script.dataset.api = "https://api.krunchdata.io/traffic/web/record";
-      script.dataset.id = "VVQmI5b3tzqkIee4pzhoyG5gXFHnbnmFAjjaV1flsqyTAIw91uf1g1i7YN1VkHpB";
-      script.defer = true;
-      script.type = "module";
-
-      document.head.appendChild(script);
-      console.log("Krunch script installed");
-    </script>
+    <script
+      src="https://app.krunchdata.io/assets/js/k2.js"
+      data-api="https://api.krunchdata.io/traffic/web/record"
+      dataset-api="https://api.krunchdata.io/traffic/web/record"
+      dataset-id="VVQmI5b3tzqkIee4pzhoyG5gXFHnbnmFAjjaV1flsqyTAIw91uf1g1i7YN1VkHpB"
+      data-id="VVQmI5b3tzqkIee4pzhoyG5gXFHnbnmFAjjaV1flsqyTAIw91uf1g1i7YN1VkHpB"
+      defer
+      type="module"
+    />
     <GoogleAnalytics trackPageViews />
     <Component {...pageProps} />
   </>;
